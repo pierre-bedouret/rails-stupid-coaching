bd8025b09f88261ae72b2081abf802821b8387b8
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-11-11 14:07:19 +0100

    stupid coaching : Done

---
+++ test/system/questions_test.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class QuestionsTest < ApplicationSystemTestCase
+  test 'visiting /ask renders the form' do
+    visit ask_url
+    assert_selector 'p', text: 'Ask your coach anything'
+  end
+
+  test 'saying Hello yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello'
+    click_on 'Ask'
+
+    assert_text 'I don\'t care, get dressed and go to work!'
+  end
+
+  test 'saying How are you ? yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'How are you ?'
+    # take_screenshot
+    click_on 'Ask'
+
+    assert_text 'Silly question, get dressed and go to work!'
+    # take_screenshot
+  end
+end
